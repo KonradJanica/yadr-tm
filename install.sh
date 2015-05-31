dabbe6553705f96a3e0fdae15b3d712f9511c720
Tullie Murrell <konradjanica@gmail.com>
2015-06-01 02:26:29 +0930

    fixed script

--- install.sh
+++ install.sh
@@ -1,36 +1,47 @@
 #!/bin/sh
 
 if [ ! -d "$HOME/.yadr" ]; then
-    echo "Installing YADR-TM for the first time"
-    echo "Patching .bashrc ..."
-    cat .bashrc >> "
-# Copy working directory
-if [ -e /dev/clipboard ]; then
-   alias pbcopy='cat >/dev/clipboard'
-      alias pbpaste='cat /dev/clipboard'
-fi
+  echo "Installing YADR-TM for the first time"
+  echo "Patching .bashrc ..."
+  echo "# Copy working directory
+  if [ -e /dev/clipboard ]; then
+    alias pbcopy='cat >/dev/clipboard'
+    alias pbpaste='cat /dev/clipboard'
+  fi
 
-alias cpwd="pwd | tr -d '\n' | pbcopy"
+  alias cpwd=\"pwd | tr -d '\n' | pbcopy\"
 
-# Go shortcuts
-alias gointerview="cd /cygdrive/c/aPrograming/interview/"
-alias gocg="cd /cygdrive/c/aPrograming/svn/a1194898/2015/s1/cg/assignment2/"
-alias gocna="cd /cygdrive/c/aPrograming/svn/a1194898/2015/s1/cna/AlternatingBit"
+  # Go shortcuts
+  alias gointerview=\"cd /cygdrive/c/aPrograming/interview/\"
+  alias gocg=\"cd /cygdrive/c/aPrograming/svn/a1194898/2015/s1/cg/assignment2/\"
+  alias gocna=\"cd /cygdrive/c/aPrograming/svn/a1194898/2015/s1/cna/AlternatingBit\"
 
-# LS color on
-alias ls="ls --color=always"
+  # LS color on
+  alias ls=\"ls --color=always\"
 
-# Screen Cygwin hack
-alias screen="rm -rf /tmp/uscreens/; screen"
+  # Screen Cygwin hack
+  alias screen=\"rm -rf /tmp/uscreens/; screen\"
 
-# Use experimental opengl
-export LIBGL_USE_WGL=1
-# export LIBGL_ALWAYS_INDIRECT=1"
+  # Use experimental opengl
+  export LIBGL_USE_WGL=1
+  # export LIBGL_ALWAYS_INDIRECT=1" >> .bashrc
 
-    git clone https://github.com/KonradJanica/yadr-tm "$HOME/.yadr"
-    cd "$HOME/.yadr"
-    [ "$1" = "ask" ] && export ASK="true"
-    rake install
+  cd /etc
+  echo "# Disable vim scrollback buffer
+  altscreen on
+
+  # bottom toolbar
+  autodetach on 
+  startup_message off 
+  hardstatus alwayslastline 
+  shelltitle 'bash'
+
+  hardstatus string '%{gk}[%{wk}%?%-Lw%?%{=b kR}(%{W}%n*%f %t%?(%u)%?%{=b kR})%{= w}%?%+Lw%?%? %{g}][%{d}%l%{g}][ %{= w}%Y/%m/%d %0C:%s%a%{g} ]%{W}'" >> screenrc
+
+  git clone https://github.com/KonradJanica/yadr-tm "$HOME/.yadr"
+  cd "$HOME/.yadr"
+  [ "$1" = "ask" ] && export ASK="true"
+  rake install
 else
-    echo "YADR is already installed"
+  echo "YADR is already installed"
 fi
