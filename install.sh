f5ecfd97a3113edd9c917743a22c8dd32d5e9e4a
Tullie Murrell <konradjanica@gmail.com>
2015-06-01 02:50:30 +0930

    cleaned up patching

--- install.sh
+++ install.sh
@@ -3,40 +3,43 @@
 if [ ! -d "$HOME/.yadr" ]; then
   echo "Installing YADR-TM for the first time"
   echo "Patching .bashrc ..."
-  echo "# Copy working directory
-  if [ -e /dev/clipboard ]; then
-    alias pbcopy='cat >/dev/clipboard'
-    alias pbpaste='cat /dev/clipboard'
-  fi
+  echo "
+# Copy working directory
+if [ -e /dev/clipboard ]; then
+  alias pbcopy='cat >/dev/clipboard'
+  alias pbpaste='cat /dev/clipboard'
+fi
 
-  alias cpwd=\"pwd | tr -d '\n' | pbcopy\"
+alias cpwd=\"pwd | tr -d '\n' | pbcopy\"
 
-  # Go shortcuts
-  alias gointerview=\"cd /cygdrive/c/aPrograming/interview/\"
-  alias gocg=\"cd /cygdrive/c/aPrograming/svn/a1194898/2015/s1/cg/assignment2/\"
-  alias gocna=\"cd /cygdrive/c/aPrograming/svn/a1194898/2015/s1/cna/AlternatingBit\"
+# Go shortcuts
+alias gointerview=\"cd /cygdrive/c/aPrograming/interview/\"
+alias gocg=\"cd /cygdrive/c/aPrograming/svn/a1194898/2015/s1/cg/assignment2/\"
+alias gocna=\"cd /cygdrive/c/aPrograming/svn/a1194898/2015/s1/cna/AlternatingBit\"
 
-  # LS color on
-  alias ls=\"ls --color=always\"
+# LS color on
+alias ls=\"ls --color=always\"
 
-  # Screen Cygwin hack
-  alias screen=\"rm -rf /tmp/uscreens/; screen\"
+# Screen Cygwin hack
+alias screen=\"rm -rf /tmp/uscreens/; screen\"
 
-  # Use experimental opengl
-  export LIBGL_USE_WGL=1
-  # export LIBGL_ALWAYS_INDIRECT=1" >> .bashrc
+# Use experimental opengl
+export LIBGL_USE_WGL=1
+# export LIBGL_ALWAYS_INDIRECT=1" >> .bashrc
 
+  echo "Patching screenrc ..."
   cd /etc
-  echo "# Disable vim scrollback buffer
-  altscreen on
+  echo "
+# Disable vim scrollback buffer
+altscreen on
 
-  # bottom toolbar
-  autodetach on 
-  startup_message off 
-  hardstatus alwayslastline 
-  shelltitle 'bash'
+# bottom toolbar
+autodetach on 
+startup_message off 
+hardstatus alwayslastline 
+shelltitle 'bash'
 
-  hardstatus string '%{gk}[%{wk}%?%-Lw%?%{=b kR}(%{W}%n*%f %t%?(%u)%?%{=b kR})%{= w}%?%+Lw%?%? %{g}][%{d}%l%{g}][ %{= w}%Y/%m/%d %0C:%s%a%{g} ]%{W}'" >> screenrc
+hardstatus string '%{gk}[%{wk}%?%-Lw%?%{=b kR}(%{W}%n*%f %t%?(%u)%?%{=b kR})%{= w}%?%+Lw%?%? %{g}][%{d}%l%{g}][ %{= w}%Y/%m/%d %0C:%s%a%{g} ]%{W}'" >> screenrc
 
   git clone https://github.com/KonradJanica/yadr-tm "$HOME/.yadr"
   cd "$HOME/.yadr"
